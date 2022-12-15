7c0366b011c75f76e45489ca249e0ef72377d5c9
John Devlin <johnp.devlin2@gmail.com>
2022-12-15 13:29:05 +0000

    RDY 4 COMMIT

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/question-practice-tracker',
+	base: '/question-practice-tracker/',
 	plugins: [react()],
 });
